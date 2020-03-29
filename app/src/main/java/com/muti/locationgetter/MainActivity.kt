ca696d1cfed6dcb44df3204a4e86b65906499cc5
erolsancaktutan <erolsancaktutan@gmail.com>
2020-03-29 13:04:39 +0300

    Location data for Api> = 23 was performed

--- app/src/main/java/com/muti/locationgetter/MainActivity.kt
+++ app/src/main/java/com/muti/locationgetter/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.muti.locationgetter
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import com.muti.location.MutiLocation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var mutiLocation:MutiLocation?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mutiLocation = MutiLocation(this)
+
+        mutiLocation!!.getCurrentLocation().observe(this, Observer { t ->
+            btn.text = "Lat: "+t.latitude.toString()+ "\nLong: "+t.longitude.toString()
+        })
+
+        btn.setOnClickListener(View.OnClickListener {
+            getLocation()
+        })
+    }
+
+    private fun getLocation(){
+        mutiLocation!!.getLocation()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1453-> {
+                if (grantResults.isNotEmpty() &&
+                    mutiLocation!!.hasAllPermissionsGranted(grantResults)) {
+                   getLocation()
+                } else {
+
+                }
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
---
+++ location/src/main/java/com/muti/location/MutiLocation.kt
@@ -0,0 +1,151 @@
+package com.muti.location
+
+import android.Manifest
+import android.app.Activity
+import android.app.Service
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.os.IBinder
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+
+class MutiLocation(private val activity: Activity): Service(), LocationListener {
+    private var isGPSEnabled = false
+    private var isNetworkEnabled = false
+    private var canGetLocation = false
+    private var mLocation: Location?=null
+    private var mutableLocation= MutableLiveData<Location>()
+    private lateinit var locationManager: LocationManager
+    private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Float =
+        10f // The minimum distance to change Updates in meters
+    private val MIN_TIME_BW_UPDATES: Long =
+        1000 * 60 * 1 // The minimum time between updates in milliseconds
+
+    companion object {
+        private var INSTANCE: MutiLocation? = null
+        fun getInstance(activity: Activity): MutiLocation {
+            if (INSTANCE == null) {
+                INSTANCE = MutiLocation(activity)
+            }
+            return INSTANCE!!
+        }
+    }
+
+    private fun isReadyLocationService(): Boolean {
+        locationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        canGetLocation = isGPSEnabled || isNetworkEnabled
+        return canGetLocation
+    }
+
+    fun getLocation() {
+        if (isReadyLocationService()) {
+            if (isNetworkEnabled || isGPSEnabled) {
+                checkOfPermissions()
+            }
+        } else {
+            showSettingsAlert()
+        }
+    }
+
+    private fun checkOfPermissions() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    activity, arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    1453
+                )
+            }
+            else {
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this
+                    )
+                    if (locationManager != null) {
+                        mLocation =
+                            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+                    }
+                }else if (isGPSEnabled) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        mLocation = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    }
+                }
+            }
+        }
+    }
+    fun getCurrentLocation():MutableLiveData<Location>{
+        return mutableLocation
+    }
+
+    private fun showSettingsAlert() {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(activity)
+        alertDialog.setTitle("GPS is settings")
+        alertDialog
+            .setMessage("GPS is not enabled. Do you want to go to settings menu?")
+
+        alertDialog.setPositiveButton("Settings",
+            DialogInterface.OnClickListener { dialog, which ->
+                activity.startActivity(
+                    Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                )
+            })
+        alertDialog.setNegativeButton("Cancel",
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+        alertDialog.show()
+    }
+
+    fun hasAllPermissionsGranted(grantResults:IntArray): Boolean {
+        for (result in grantResults){
+            if (result == PackageManager.PERMISSION_DENIED)
+                return false
+        }
+        return true
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        mutableLocation.value=location
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+    override fun onProviderEnabled(provider: String?) {}
+
+    override fun onProviderDisabled(provider: String?) {}
+}
